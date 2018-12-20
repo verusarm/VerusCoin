bde2c9e6937cc069cf7997b4d55e85d228d138b6
Asher Dawes <asher.dawes@gmail.com>
2018-12-20 12:43:36 -0800

    Updating Agama upgrade script

--- zcutil/upgrade-agama.sh
+++ zcutil/upgrade-agama.sh
@@ -4,16 +4,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 if [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Removing old binaries"
-    rm /Applications/Agama.app/Contents/Resources/app/assets/bin/osx/*
+    rm /Applications/Agama.app/Contents/Resources/app/assets/bin/osx/verusd/*
     echo "Copying files"
-    cp ./* /Applications/Agama.app/Contents/Resources/app/assets/bin/osx
+    cp ./* /Applications/Agama.app/Contents/Resources/app/assets/bin/osx/verusd/
     echo "Upgrade complete"
 else
     echo "Agama directory, followed by [ENTER]:"
     read AGAMA_DIR
     echo "Removing old binaries"
-    rm ${AGAMA_DIR}/resources/app/assets/bin/linux64/*
+    rm ${AGAMA_DIR}/resources/app/assets/bin/linux64/verusd/*
     echo "Copying files"
-    cp ./* ${AGAMA_DIR}/resources/app/assets/bin/linux64
+    cp ./* ${AGAMA_DIR}/resources/app/assets/bin/linux64/verusd/
     echo "Upgrade complete"
 fi
