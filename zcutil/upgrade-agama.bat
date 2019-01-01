b65ded6a881acd2beab51fb43b8d66288dd744f5
Asher Dawes <asher.dawes@gmail.com>
2019-01-01 00:00:02 -0800

    Fixing cmd script

--- zcutil/upgrade-agama.bat
+++ zcutil/upgrade-agama.bat
@@ -3,8 +3,8 @@ call :GET_CURRENT_DIR
 cd %THIS_DIR%
 set /p AGAMA_DIR="Agama directory, followed by [ENTER]:"
 echo "Removing old binaries"
-rmdir /s %AGAMA_DIR%\resources\app\assets\bin\win64\verusd\verusd
-MKDIR %AGAMA_DIR%\resources\app\assets\bin\win64\verusd\verusd
+rmdir /s %AGAMA_DIR%\resources\app\assets\bin\win64\verusd
+MKDIR %AGAMA_DIR%\resources\app\assets\bin\win64\verusd
 echo "Copying files"
 xcopy /E %AGAMA_DIR%\resources\app\assets\bin\win64\verusd
 ren %AGAMA_DIR%\resources\app\assets\bin\win64\verusd\komodod.exe %AGAMA_DIR%\resources\app\assets\bin\win64\verusd\verusd.exe
