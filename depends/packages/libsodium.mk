5635677fb2a04d9344398eefc76f13b98ee8cf86
Asher Dawes <asher.dawes@gmail.com>
2019-01-10 00:06:47 -0800

    Update libsodium.mk (#190)

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -5,7 +5,7 @@ $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=fb6a9e879a2f674592e4328c5d9f79f082405ee4bb05cb6e679b90afe9e178f4
 $(package)_dependencies=
 $(package)_config_opts=
-endif
+
 
 define $(package)_preprocess_cmds
   cd $($(package)_build_subdir); ./autogen.sh
