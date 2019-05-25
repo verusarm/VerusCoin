4d4d75694d6d6ed7f270ad2e2ab65ce10fcd4a27
Michael Toutonghi <michaeltoutonghi@gmail.com>
2019-05-24 23:44:56 -0700

    Additional checking

--- src/rpc/pbaasrpc.cpp
+++ src/rpc/pbaasrpc.cpp
@@ -839,7 +839,7 @@ UniValue submitacceptednotarization(const UniValue& params, bool fHelp)
         for (auto input : notarizationInputs)
         {
             LogPrintf("Accepted notarization value: %lu, input n: %d, hash: %s\n", input.nValue, input.txIn.prevout.n, input.txIn.prevout.hash.GetHex().c_str());
-            printf("Accepted notarization value: %lu, input n: %d, hash: %s\n", input.nValue, input.txIn.prevout.n, input.txIn.prevout.hash.GetHex().c_str());
+            //printf("Accepted notarization value: %lu, input n: %d, hash: %s\n", input.nValue, input.txIn.prevout.n, input.txIn.prevout.hash.GetHex().c_str());
         }
 
         // if we got our inputs, add finalization
